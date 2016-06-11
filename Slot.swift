2d2cd36ae35da53c64ac52db537199b1a7998d47
Lucas Maciel <manobray@iMac-de-Lucas.local>
2016-06-10 23:11:11 -0300

    initial commit

---
+++ Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Lucas Maciel on 7/26/15.
+//  Copyright (c) 2015 Lucas Maciel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot{
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
